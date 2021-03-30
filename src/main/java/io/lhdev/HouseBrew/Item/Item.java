6fc6f46a33ea6eb209ab7d7d3fb8305af3fd64fe
Leah H <harrichl@gmail.com>
2021-03-30 14:47:36 -0400

    create Item class

---
+++ src/main/java/io/lhdev/HouseBrew/Item/Item.java
@@ -0,0 +1,89 @@
+package io.lhdev.HouseBrew.Item;
+
+public class Item {
+    private Long id;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private int size;
+    private float price;
+
+    public Item() {
+    }
+
+    public Item(Long id, String name, String description, String imageUrl, int size, float price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.size = size;
+        this.price = price;
+    }
+
+    public Item(String name, String description, String imageUrl, int size, float price) {
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.size = size;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", size=" + size +
+                ", price=" + price +
+                '}';
+    }
+}
