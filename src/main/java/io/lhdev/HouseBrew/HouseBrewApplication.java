a51f4ee76a4fe07269cf8f849a5e8f8e8c44d9c9
Leah H <harrichl@gmail.com>
2021-03-31 01:07:12 -0400

    add ItemRepository interface :

--- src/main/java/io/lhdev/HouseBrew/HouseBrewApplication.java
+++ src/main/java/io/lhdev/HouseBrew/HouseBrewApplication.java
@@ -1,12 +1,7 @@
 package io.lhdev.HouseBrew;
 
-import io.lhdev.HouseBrew.item.Item;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 public class HouseBrewApplication {
--- src/main/java/io/lhdev/HouseBrew/item/ItemService.java
+++ src/main/java/io/lhdev/HouseBrew/item/ItemService.java
@@ -1,6 +1,6 @@
 package io.lhdev.HouseBrew.item;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,22 +8,14 @@ import java.util.List;
 @Service
 public class ItemService {
 
-    public List<Item> getItems(){
-        return List.of(
-                new Item(
-                        1L,
-                        "Giant Steps",
-                        "Coltrane’s Giant Steps, this dense and substantial coffee is not unlike the jazz visionary's " +
-                                "signature sheets of sound. Our darkest blend, comprising organic coffees from Uganda," +
-                                "Papua New Guinea, and Sumatra, is downright viscous in the cup. Just like Coltrane’s " +
-                                "ability to cascade into high-pitched octaves with maximum control, this coffee’s " +
-                                "inflections of stone fruit lighten without losing focus. Improvisations of milk or " +
-                                "cream—in any proportion—shine.",
-                        "https://images.unsplash.com/photo-1550681560-af9bc1cb339e?ixlib=rb-1.2.1" +
-                                "&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-                        12,
-                        10L)
+    private final ItemRepository itemRepository;
 
-        );
+    @Autowired
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public List<Item> getItems(){
+        return itemRepository.findAll();
     }
 }
